9cd8217e8dab911e67b0048671f76a006fa30ac9
khoidd <khoidd@outlook.com>
2021-08-26 20:14:26 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Collapse, Button, Col, Row } from 'antd';
 import './App.css';
 
-function App() {
+import { useReactMediaRecorder } from "react-media-recorder";
+const App = () => {
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ video: true });
+
+  console.info(status)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Collapse defaultActiveKey={['1']}>
+      <Collapse.Panel header="Demo hand control" key="1">
+        <Button type="primary">LEFT</Button>
+        <Button type="primary">RIGHT</Button>
+        <Button type="primary">FORWARD</Button>
+        <Button type="primary">BACKWARD</Button>
+      </Collapse.Panel>
+      <Collapse.Panel header="Demo voice control" key="2">
+        <Row>
+          <Col span={8}>
+            {
+              status == "recording"
+                ? <Button style={{ marginTop: "15px" }} type="primary" onClick={stopRecording}>Stop</Button>
+                : <Button style={{ marginTop: "15px" }} type="primary" onClick={startRecording}>Record</Button>
+            }
+          </Col>
+          <Col span={16}>
+            <audio src={mediaBlobUrl} controls autoPlay loop />
+          </Col>
+        </Row>
+      </Collapse.Panel>
+    </Collapse>
+  )
 }
+
 
 export default App;
