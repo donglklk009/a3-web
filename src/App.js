4c6476bf04a74abeaa7cca1e2f51be57107a8c5e
donglklk009 <69371899+donglklk009@users.noreply.github.com>
2022-05-25 22:09:48 +0700

    update voice_speed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Collapse, Button, Col, Row, notification } from 'antd';
+import { Collapse, Button, Col, Row,Slider, notification } from 'antd';
 import './App.css';
 import axios from 'axios';
 import { useReactMediaRecorder } from "react-media-recorder";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
-const URL_A3_SERVICE = 'http://localhost:8080'
+const URL_A3_SERVICE = 'http://192.168.1.6:8080'
 const App = () => {
   const {
     status,
@@ -13,29 +14,55 @@ const App = () => {
     mediaBlobUrl,
   } = useReactMediaRecorder({ video: true });
 
-  const sendHandControl = (direction, speed) => {
+  const sendHandControl = (direction) => {
     console.info(direction)
     axios.post(`${URL_A3_SERVICE}/hand`, {
-      "direction": direction,
-      "speed": speed
+      "direction": 'direction',
+      "speed": 'speed'
     }).then((data) => {
-      notification.open({
-        message: 'Notification Title',
-        description: "ok"
+      console.open({
+        message: direction,
+        description: status
       })
     }).catch((error) => {
       console.info(error)
     })
   }
 
+  const sendSpeedControl = (speed) => {
+    console.info(speed)
+    axios.post(`${URL_A3_SERVICE}/speed`, {
+      "direction": 'direction',
+      "speed": 'speed'
+    }).then((data) => {
+      console.open({
+        message: 'direction',
+        description: status
+      })
+    }).catch((error) => {
+      console.info(error)
+    })
+  }
+
+
   return (
     <Collapse defaultActiveKey={['1']}>
       <Collapse.Panel header="Hand control" key="1">
-        <Button onClick={() => sendHandControl(1, 1)} type="primary">LEFT</Button>
+        <Button onMouseDown={() => sendHandControl(1)} 
+                type="primary"
+                onMouseUp={() => sendHandControl(5)} > 
+        LEFT
+        </Button>
         <Button onClick={() => sendHandControl(2, 1)} type="primary">RIGHT</Button>
         <Button onClick={() => sendHandControl(3, 1)} type="primary">FORWARD</Button>
         <Button onClick={() => sendHandControl(4, 1)} type="primary">BACKWARD</Button>
       </Collapse.Panel>
+
+
+
+
+
+
       <Collapse.Panel header="Voice control" key="2">
         <Row>
           <Col span={8}>
